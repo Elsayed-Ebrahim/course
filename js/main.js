b7f82c12ef3062ff982622e191d9ec75bd80a5b1
elsayed ebrahim <ee705190@gmail.com>
2024-07-24 19:20:49 +0300

    first coomit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function show(){
+    alert("welcome");
+}
+show();
+
+function result(...numbers){
+  
+    let sum=0;
+    for( let i=0 ; i<numbers.length; i++){
+        sum=sum+numbers[i];
+    }
+
+    return sum;}
+ alert(result(4,7,9))
+
+
+
+
+ 
+ let x=[4,5,6,7];
+ for(let j=0; j<x.length;j++){
+    total=total+x[j];
+    total+=x[j];
+ }
+
+
+ // return' the result is:${sum}';
+
+
+    
+
